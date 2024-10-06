a6ba463ee0c6f38ccfd977552b875eefadb0be54
Julio Barriga <mushachi@gmail.>
2024-10-06 13:46:23 -0600

    País y Región

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
-  selector: 'app-by-region-page',
+  selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
 export class ByRegionPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService ) {}
+
+  searchByRegion( term: string ): void{
+    // console.log('Desde byCapitalPage');
+    // console.log({ term });
+    this.countriesService.searchRegion( term )
+      .subscribe( countries => {
+        this.countries = countries;
+        // console.log( this.countries[8].translations['spa'].common );
+    });
+
+  }
 
 }
