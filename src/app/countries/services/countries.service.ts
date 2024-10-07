8e2e9dadd86cdce61a4ec0d4d13d0b047bbb6371
Julio Barriga <mushachi@gmail.>
2024-10-06 19:07:55 -0600

    switchMap - Refactorización

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of, tap } from 'rxjs';
+import { catchError, Observable, of, map } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -10,6 +10,15 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
   constructor( private http: HttpClient ) { }
+
+  searchCountryByAlphaCode( code: string ): Observable<Country | null> {
+    const url = `${ this.apiUrl }/alpha/${ code }`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0]: null ),
+        catchError( () => of(null) )
+      );
+  }
 
   searchCapital( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`;
