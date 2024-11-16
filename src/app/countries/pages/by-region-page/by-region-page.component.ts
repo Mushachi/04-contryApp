5ba2dc877d1906467560b60fde9bf3ccbbbfc670
Julio Barriga <mushachi@gmail.>
2024-11-16 16:53:53 -0600

    Buscar por region - Optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europa' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,13 +12,18 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] =  [ 'Africa', 'America', 'Asia', 'Europa', 'Oceania' ] ;
+  public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService ) {}
 
-  searchByRegion( term: string ): void{
+  searchByRegion( region: Region ): void{
     // console.log('Desde byCapitalPage');
     // console.log({ term });
-    this.countriesService.searchRegion( term )
+
+    this.selectedRegion = region;
+
+    this.countriesService.searchRegion( region )
       .subscribe( countries => {
         this.countries = countries;
         // console.log( this.countries[8].translations['spa'].common );
